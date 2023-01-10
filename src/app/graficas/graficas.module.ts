88df8016a366757506c647ed5d2730120ccc4a30
XFabio22 <98523846+XFabio22@users.noreply.github.com>
2023-01-10 10:51:16 +0100

    Estructura del proyecto

---
+++ src/app/graficas/graficas.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { GraficasRoutingModule } from './graficas-routing.module';
+import { BarrasComponent } from './pages/barras/barras.component';
+import { BarrasDoblesComponent } from './pages/barras-dobles/barras-dobles.component';
+import { DonaHttpComponent } from './pages/dona-http/dona-http.component';
+import { GraficaBarraComponent } from './components/grafica-barra/grafica-barra.component';
+
+
+@NgModule({
+  declarations: [
+    BarrasComponent,
+    BarrasDoblesComponent,
+    DonaHttpComponent,
+    GraficaBarraComponent
+  ],
+  imports: [
+    CommonModule,
+    GraficasRoutingModule
+  ]
+})
+export class GraficasModule { }
