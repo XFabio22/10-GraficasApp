1fc71b67e1177f762a7b7665826e5e1b22143bed
XFabio22 <98523846+XFabio22@users.noreply.github.com>
2023-01-13 10:29:23 +0100

    Personalizando la grafica

--- src/app/graficas/pages/barras/barras.component.ts
+++ src/app/graficas/pages/barras/barras.component.ts
@@ -13,30 +13,32 @@ export class BarrasComponent {
   public barChartOptions: ChartConfiguration['options'] = {
     responsive: true,
     // We use these empty structures as placeholders for dynamic theming.
-    scales: {
-      x: {},
-      y: {
-        min: 10
-      }
-    },
-    plugins: {
-      legend: {
-        display: true,
-      },
-      // datalabels: {
-      //   anchor: 'end',
-      //   align: 'end'
-      // }
-    }
+    // scales: {
+    //   x: {},
+    //   y: {
+    //     min: 10
+    //   }
+    // },
+    // plugins: {
+    //   legend: {
+    //     display: true,
+    //   },
+    //   // datalabels: {
+    //   //   anchor: 'end',
+    //   //   align: 'end'
+    //   // }
+    // }
   };
   public barChartType: ChartType = 'bar';
 
 
   public barChartData: ChartData<'bar'> = {
-    labels: [ '2006', '2007', '2008', '2009', '2010', '2011', '2012' ],
+    labels: [ '2020', '2021', '2022', '2023', '2024', '2025', '2026','2027' ],
     datasets: [
-      { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A' },
-      { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B' }
+      { data: [ 65, 59, 80, 81, 56, 55, 40 ], label: 'Series A',backgroundColor:'#FA0D21',hoverBackgroundColor:'blue'},
+      { data: [ 28, 48, 40, 19, 86, 27, 90 ], label: 'Series B',backgroundColor:'#FA910D',hoverBackgroundColor:'blue'  },
+      { data: [ 8, 48, 4, 19, 6, 27, 99 ], label: 'Series C',backgroundColor:'#DE620B',hoverBackgroundColor:'blue'  }
+
     ]
   };
 
@@ -51,14 +53,36 @@ export class BarrasComponent {
 
   public randomize(): void {
     // Only Change 3 values
-    this.barChartData.datasets[0].data = [
+    this.barChartData.datasets[0].data = [ //SERIE A
       Math.round(Math.random() * 100),
-      59,
-      80,
       Math.round(Math.random() * 100),
-      56,
       Math.round(Math.random() * 100),
-      40 ];
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+
+      ];
+      this.barChartData.datasets[1].data = [ //SERIE A
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+
+      ];
+      this.barChartData.datasets[2].data = [ //SERIE A
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+      Math.round(Math.random() * 100),
+
+      ];
 
     this.chart?.update();
   }
